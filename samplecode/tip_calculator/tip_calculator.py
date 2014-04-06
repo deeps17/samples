4f799c08349164169946b159c61a099eddfc25ff
Deepti Gottipati <gdeepti@gmail.com>
2014-04-06 14:57:17 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   tip_calculator.py
    
    This version of tip_calculator takes user input using raw_input()

--- samplecode/tip_calculator/tip_calculator.py
+++ samplecode/tip_calculator/tip_calculator.py
@@ -1,8 +1,12 @@
 #! /usr/bin/python
 
-meal = 50
-tax = 6
-tip = 15
+#meal = 50
+#tax = 6
+#tip = 15
+meal = float( raw_input("Enter the value of your meal (eg: 33.50 etc): "))
+tax = int( raw_input("Enter the tax percent (eg: 3,5 and not 3%,5%): "))
+tip = int( raw_input("Enter the tip percent (eg: 10,15 and not 10%, 15%): "))
+
 
 tax_value = (meal * tax / 100)
 meal_with_tax = meal + tax_value
@@ -11,6 +15,6 @@ tip_value = (meal_with_tax * tip ) / 100
 total = meal_with_tax + tip_value
 
 print "The base cost of your meal was $%s" % meal 
-print "You need to pay $ %.2f" % tax_value , " for tax."
-print "Tipping at a rate of %s" % tip , '%%, you should leave $ %.2f' % tip_value , ' for a tip.'
-print "The grand total of your meal is $%.2f" % total
+print "You need to pay ${0:.2f} for tax.".format(tax_value)
+print "Tipping at a rate of {0}%, you should leave ${1:.2f} for a tip.".format(tip,tip_value)
+print "The grand total of your meal is ${0:.2f}".format(total)
